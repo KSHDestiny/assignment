c4b2b1955b6de5bcd1f93cff4668c3a60b316144
KaungSatHein <kaungsathein.5899@gmail.com>
2023-02-10 05:07:42 +0630

    Good Luck

---
+++ week5/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kaung Sat Hein PHP</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <h1>Kaung Sat Hein PHP</h1>
+    <p>The SHA256 hash of "Kaung Sat Hein" is <?php print hash('sha256','Kaung Sat Hein'); ?></p>
+
+    <p>ASCII ART:</p>
+    <pre>
+    **           **
+    **         **
+    **       **
+    **     **
+    **  **
+    ** **
+    **  **
+    **     **
+    **       **
+    **         **
+    **           **
+    </pre>
+
+    <a href="check.php">Click here to check the error setting</a> <br>
+    <a href="fail.php">Click here to cause a traceback</a>
+</body>
+
+</html>
